01acefbf56901a68d5e1a2f4fb87782e998917ad
kanhairaval <kanhairaval@gmail.com>
2023-02-21 01:15:30 -0500

    same as before, in terms of error

--- generateQuestions.js
+++ generateQuestions.js
@@ -174,19 +174,3 @@ function generateQuestions () {
 generateQuestions(db);
 
 module.exports = generateQuestions;
-
-// SELECT 
-//           employees.employee_id AS id, 
-//           employees.first_name, 
-//           employees.last_name, 
-//           roles.job_title AS title, 
-//           departments.department_name AS department, 
-//           roles.role_salary AS salary, 
-//           CONCAT(managers.first_name, ' ', managers.last_name) AS manager 
-//           FROM 
-//           employees 
-//           JOIN roles ON employees.id_role = roles.role_id 
-//           JOIN departments ON roles.dep_id = departments.id 
-//           LEFT JOIN employees AS managers ON employees.manager_id = managers.employee_id 
-//           WHERE
-//           employees.employee_id = LAST_INSERT_ID();
